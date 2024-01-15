9093657deb845eeb2e8af6400607b14edffdd4b5
sssseungk <cacy0118@naver.com>
2024-01-15 13:00:17 +0900

    🔨 as를 활용한 컴포넌트 태그 변경(style 유지), attrs를 활용한 컴포넌트 속성값 설정하기

--- src/App.js
+++ src/App.js
@@ -7,26 +7,39 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  // 컴포넌트에서 props 받기
-  background-color: ${(props) => props.bgColor}; 
-  width: 100px; 
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-// Box의 모든 속성을 가져온다. (확장하려는 컴포넌트 이름 작성하기)
-const Circle = styled(Box)`
-  border-radius: 50px;
-`
+const Input = styled.input.attrs({required:true, minLength: 10})`
+  background-color: tomato;
+`;
 
 function App(){
   return (
-    <Father>
-      <Box bgColor="teal"/>
-      <Circle bgColor="tomato"/>
+    <Father as="header">
+      <Btn>Login</Btn>
+      {/* Btn의 style은 유지하고, html 태그를 button이 아닌 a로 설정 */}
+      <Btn as="a">Login</Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   )
 }
 
 export default App;
+
+
+// 1. 다른 컴포넌트의 스타일은 그대로, 태그만 바꾸고 싶을때 => 컴포넌트에 as="" 사용하기
+// ex. <Father as="header"/>  :: div 태그였던 컴포넌트를 스타일은 같은 header 태그로 사용
+// 2. 컴포넌트의 속성값을 설정하는 방법 => styled.태그명.attrs({속성들}) 
+// ex. const In[ut = styled.input.attrs({required: true})`CSS속성`;
+
+
 
