7d174313a16ac49a5be00f1f03f2ca5c546db7b5
sssseungk <cacy0118@naver.com>
2024-01-15 12:36:14 +0900

    🔨 styled components 활용한 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import styled from "styled-components";
+
+// styled components 활용하여 컴포넌트 생성
+// CSS 코드를 그대로 쓸 수 있다는 장점이 있음
+// className, style을 사용할 필요가 없음
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal; 
+  width: 100px; 
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
 function App(){
-  return null;
+  return (
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo/>
+    </Father>
+  )
 }
 
 export default App;
