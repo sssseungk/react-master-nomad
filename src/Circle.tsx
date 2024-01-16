1d7896fa16f1f806878beb12e87e501e1c211665
sssseungk <cacy0118@naver.com>
2024-01-16 16:19:13 +0900

    🔨 required props와 optional props, optional props 기본값 설정하기

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 interface containerProps{
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<containerProps>`
@@ -9,19 +10,31 @@ const Container = styled.div<containerProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 50%;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps{
-  bgColor: string;
+  bgColor: string;        /* default props :: required 프로퍼티*/
+  borderColor?: string;   /* optional props : undefined일수도 있는 프로퍼티*/
+  text?: string;
 }
 
-function Circle({bgColor}: CircleProps){
+function Circle({bgColor, borderColor, text = "default text"}: CircleProps){
   return (
     <div>
-      <Container bgColor={bgColor}/>
+      <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+        {text}
+      </Container>
     </div>
   )
 }
 
-
 export default Circle;
+
+/*
+required props, optional props
+optional props의 기본값 설정 : borderColor ?? bgColor 
+  ㄴ borderColor가 undefined이면 borderColor 값을 bgColor 값으로 지정하기 
+* CircleProps에선 borderColor가 필수값이 아니지만 
+ContainerProps에 borderColor가 필수 속성으로 들어가 있기 때문에 default 값을 지정해줘야한다. 
+*/
