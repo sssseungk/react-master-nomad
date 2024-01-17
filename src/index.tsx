2c4638480a7860f775d6056b685ca450fd70a70f
sssseungk <cacy0118@naver.com>
2024-01-17 15:11:07 +0900

    🥅 코인 이름 클릭 시 url은 바뀌지만 상세 페이지 렌더링 되지 않는 에러 해결 - React.StrictMode -> Fragment로 대체

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ import { theme } from './theme';
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <React.StrictMode>
+  <>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </>,
 );
