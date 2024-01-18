577174365aa7fb4d9adea0b2077a9bccb97c6fe6
sssseungk <cacy0118@naver.com>
2024-01-18 10:13:48 +0900

    🔨 상세페이지의 코인정보, 가격정보 데이터 타입 interface로 정의하기 - infoData - priceData

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -33,12 +33,73 @@ interface RouteState{
   name:string;
 }
 
+// info 타입 정의하기
+// 1. console.log(infoData)로 콘솔에서 확인한 infoData 우클릭 > Store object as global variable (temp1에 객체 데이터가 저장됨)
+// 2. Object.keys(temp1)로 temp1 객체의 키값들 뽑아내기
+// 3. Object.keys(temp1).join() 으로 키들 문자열로 합친 후 interface의 id에 넣기
+// 4. Object.values(temp1).map(v => typeof v).join()로 값의 타입들 뽑아내서 interface의 value에 넣기
+interface InfoData{
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+// priceInfo 타입 정의하기
+interface PriceData{
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin(){
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const {state} = useLocation<RouteState>();
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
   
   useEffect(() => {
     (async () => {
@@ -50,7 +111,9 @@ function Coin(){
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ) .json();
       setInfo(infoData);
+      // console.log(infoData);
       setPriceInfo(priceData);
+      // console.log(priceData);
       
     })();
   })
