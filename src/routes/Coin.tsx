952cb3a91b3e28b0e84f4739523a5eec4bebf4a0
sssseungk <cacy0118@naver.com>
2024-01-18 09:37:24 +0900

    🔨 코인 상세페이지에 코인 정보, 가격 정보 데이터 받아오기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -37,7 +37,24 @@ function Coin(){
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const {state} = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
   
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        // 코인 정보 받아오기
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ) .json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      
+    })();
+  })
+
   return (
     <Container>
       <Header>
@@ -50,4 +67,3 @@ function Coin(){
 }
 
 export default Coin;
-
