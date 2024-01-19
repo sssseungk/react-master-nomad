2176cee34cd40a628b39b99f2650b4c5cb9a893b
sssseungk <cacy0118@naver.com>
2024-01-19 18:28:32 +0900

    📦 React-Helmet 설치 및 추가, react-query 일정 간격으로 데이터 받아오기 - refetchInterval

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -5,6 +5,7 @@ import Price from "./Price";
 import Chart from "./Chart";
 import { useQuery } from "react-query";
 import { fetchCoinInfo, fetchCoinTickers } from "../api";
+import { Helmet } from "react-helmet";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -152,14 +153,22 @@ function Coin(){
     ["info", coinId], 
     () => fetchCoinInfo(coinId)
   );
-  const {isLoading: tickersLoading, data: tickersData} = useQuery<PriceData>(
-    ["tickers", coinId], 
-    () =>fetchCoinTickers(coinId)
+  const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
+    ["tickers", coinId],
+    () => fetchCoinTickers(coinId),
+    {
+      refetchInterval: 5000,
+    }
   );
 
   const loading = infoLoading || tickersLoading;
   return (
     <Container>
+      <Helmet>
+        <title>
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </title>
+      </Helmet>
       <Header>
         <Title>
           {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
@@ -179,8 +188,8 @@ function Coin(){
               <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
-              <span>Open Source:</span>
-              <span>{infoData?.open_source ? "Yes" : "No"}</span>
+              <span>Price:</span>
+              <span>${tickersData?.quotes.USD.ath_price}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
